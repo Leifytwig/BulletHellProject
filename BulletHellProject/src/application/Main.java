71dcff8ae446447806b34f1838e196463adc5902
Gabe <leifytwig123@yahoo.com>
2018-01-10 22:04:35 -0600

    Getting reacquainted with JavaFX

--- BulletHellProject/src/application/Main.java
+++ BulletHellProject/src/application/Main.java
@@ -1,25 +1,36 @@
 package application;
-	
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
-
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			root.setTop(new TextField("Top")); 
+		    root.setBottom(new TextField("Bottom")); 
+		    root.setLeft(new TextField("Left")); 
+		    root.setRight(new TextField("Right")); 
+		    root.setCenter(new TextField("Center")); 
+			
+			Scene scene = new Scene(root, 1024, 576);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			scene.setFill(Color.DARKCYAN);
+			
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("BulletHellProject");
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
